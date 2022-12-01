8ac5dce465e598d4100e278edd4146bbbb6031be
Arnold Gurskis <arnoldgurskis@mpg.io>
2022-12-01 22:47:20 +0000

    fix landing initializer for the new unity config;

--- Assets/Script/LandingInitializer.cs
+++ Assets/Script/LandingInitializer.cs
@@ -1,33 +1,8 @@
 ﻿using TMI.AssetManagement;
-using TMI.ConfigManagement;
-using TMI.Core;
-using TMI.Helper;
-using TMI.LogManagement;
 using TMI.UI;
 using UnityEngine;
 
-public class LandingInitializer : BaseCacheUIMiniInitializer {
-
-    protected override void Awake() {
-
-        //config and logging initialize first, before anything else
-        ConfigManager.AddConfig<LoggingConfig>();
-
-        LoggingConfig logConfig = ConfigManager.GetConfig<LoggingConfig>();
-        Logging.output = new UnityConditionalLogOutput(logConfig);
-
-        base.Awake();
-    }
-
-	protected override void RegisterManagers(IAcquirer acquirer) {
-
-        //TODO: put this into the config file
-#if UNITY_EDITOR
-        SerializationHelper.currentFormatting = Newtonsoft.Json.Formatting.Indented;
-#endif
-
-        base.RegisterManagers(acquirer);
-    }
+public class LandingInitializer : TMI.Core.Unity.BaseInitializer {
 
     protected override IAssetGroup CreateUIAssetCache() {
         IAssetGroup assetGroup = AssetGroup.Create();
