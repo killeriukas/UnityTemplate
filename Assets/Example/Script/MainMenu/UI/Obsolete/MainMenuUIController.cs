c010de6475bdbc995714944f5130dfa5fe59717f
Arnold Gurskis <arnoldgurskis@mpg.io>
2022-12-29 02:03:43 +0000

    Reformat code on main menu ui controller;

--- Assets/Example/Script/MainMenu/UI/Obsolete/MainMenuUIController.cs
+++ Assets/Example/Script/MainMenu/UI/Obsolete/MainMenuUIController.cs
@@ -1,41 +1,44 @@
-using System;
 using TMI.Core;
 using TMI.SceneManagement;
 using TMI.UI;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MainMenuUIController : BaseUIController {
+namespace Example.UI {
+	public class MainMenuUIController : UIController {
 
-	[SerializeField]
-	private UIButton quitButton;
+		[SerializeField]
+		private UIButton quitButton;
 
-	[SerializeField]
-	private UIButton startButton;
+		[SerializeField]
+		private UIButton startButton;
 
-	private ISceneManager sceneManager;
+		private ISceneManager sceneManager;
 
-	public override void Setup(IInitializer initializer) {
-		base.Setup(initializer);
-		sceneManager = initializer.GetManager<ISceneManager>();
+		public override void Setup(IInitializer initializer) {
+			base.Setup(initializer);
+			sceneManager = initializer.GetManager<ISceneManager>();
 
-		quitButton.onButtonClick += OnQuitClicked;
-		startButton.onButtonClick += OnStartClicked;
-	}
+			quitButton.onButtonClick += OnQuitClicked;
+			startButton.onButtonClick += OnStartClicked;
+		}
 
-	private void OnStartClicked(PointerEventData data) {
-		LoadingScreenUIController loadingScreenUIController = uiManager.LoadUI<LoadingScreenUIController>();
-		loadingScreenUIController.Show();
+		private void OnStartClicked(PointerEventData data) {
+			LoadingScreenUIController loadingScreenUIController = uiManager.Load<LoadingScreenUIController>();
+			loadingScreenUIController.Show();
 
-		sceneManager.LoadAsync(SceneConstant.game);
-	}
+			sceneManager.LoadAsync(SceneConstant.game);
+		}
 
-	private void OnQuitClicked(PointerEventData data) {
-		Application.Quit();
-	}
+		private void OnQuitClicked(PointerEventData data) {
+			Application.Quit();
+		}
 
-	protected override void OnDestroy() {
-		quitButton.onButtonClick -= OnQuitClicked;
-		base.OnDestroy();
+		protected override void OnDestroy() {
+			quitButton.onButtonClick -= OnQuitClicked;
+			base.OnDestroy();
+		}
 	}
 }
+
+
